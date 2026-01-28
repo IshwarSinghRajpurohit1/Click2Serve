3c7eb38e753b70bb42ee4fb3142eca620a370a19
Ishwar Singh <ishwarsinghrajpurohit21@gmail.com>
2026-01-28 15:03:33 +0530

    My third Commit

---
+++ src/main/java/com/Click2Serve/Repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.Click2Serve.Repository;
+
+import jakarta.persistence.criteria.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByRoomId(Long roomId);
+}
+
